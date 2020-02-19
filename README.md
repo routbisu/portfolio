7ad3f5a2e9b5db00f20e2cc536abe368c6e47a04
Biswaranjan Rout <biswaranjan.rout1989@gmail.com>
2020-02-20 01:10:48 +0530

    First commit

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
-## Biswaranjan Rout | Web developer | Frontend Developer | Full Stack Developer
+# Biswaranjan Rout
+
+I am a self-motivated, self-taught web developer, driven to build amazing software that I am proud of.
+Here is my portfolio link - https://routbisu.github.io/portfolio
+
+## Sections
+
+1. Home
+2. Skills
+3. Projects
+4. Timeline
+5. Links
+6. Blog
+7. Contact
 
 ## Available Scripts
 
@@ -6,8 +19,6 @@ In the project directory, you can run:
 
 ### `yarn start`
 
-Runs the app in the development mode.<br />
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+Runs the app in the development mode.<br  />
 
-The page will reload if you make edits.<br />
-You will also see any lint errors in the console.
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React from 'react';
+import './scss/layout.scss';
 
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div className="main-container">
+      <div className="main-menu">
+        <div className="corner-home"></div>
+        <div className="horizontal-link link-1">
+          <div className="content">
+            <div className="text">Skills</div>
+          </div>
+        </div>
+        <div className="horizontal-link link-2">
+          <div className="content">
+            <div className="text">Projects</div>
+          </div>
+        </div>
+        <div className="horizontal-link link-3">
+          <div className="content">
+            <div className="text">Timeline</div>
+          </div>
+        </div>
+        <div className="horizontal-link link-4">
+          <div className="content">
+            <div className="text">Links</div>
+          </div>
+        </div>
+        <div className="horizontal-link link-5">
+          <div className="content">
+            <div className="text">Contact</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
