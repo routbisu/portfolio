d4eaf196000985a5eb9f0e3d0ceb1ec69da941e1
Biswaranjan Rout <biswaranjan.rout1989@gmail.com>
2020-02-20 01:24:57 +0530

    Fine tuning

--- src/App.js
+++ src/App.js
@@ -5,28 +5,28 @@ const App = () => {
   return (
     <div className="main-container">
       <div className="main-menu">
-        <div className="corner-home"></div>
-        <div className="horizontal-link link-1">
+        <div className="vertical-link link-1">
           <div className="content">
             <div className="text">Skills</div>
           </div>
         </div>
-        <div className="horizontal-link link-2">
+        <div className="vertical-link link-2">
           <div className="content">
             <div className="text">Projects</div>
           </div>
         </div>
-        <div className="horizontal-link link-3">
+        <div className="corner-home"></div>
+        <div className="vertical-link link-3">
           <div className="content">
             <div className="text">Timeline</div>
           </div>
         </div>
-        <div className="horizontal-link link-4">
+        <div className="vertical-link link-4">
           <div className="content">
             <div className="text">Links</div>
           </div>
         </div>
-        <div className="horizontal-link link-5">
+        <div className="vertical-link link-5">
           <div className="content">
             <div className="text">Contact</div>
           </div>
