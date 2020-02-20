182f85e5fcd01a48905dab6e1cfe569826f8f685
Biswaranjan Rout <biswaranjan.rout1989@gmail.com>
2020-02-20 17:16:30 +0530

    Updates

---
+++ precache-manifest.b71e659e158030e18d751f98e4f4fba9.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0042f4424acc708ea3ad55edde3c10a6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "05427672d9f39cf2549c",
+    "url": "/portfolio/static/css/main.4494fc69.chunk.css"
+  },
+  {
+    "revision": "39c485f86da8b1343559",
+    "url": "/portfolio/static/js/2.5fa34100.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.5fa34100.chunk.js.LICENSE"
+  },
+  {
+    "revision": "05427672d9f39cf2549c",
+    "url": "/portfolio/static/js/main.eb2ce811.chunk.js"
+  },
+  {
+    "revision": "16c109c584451046bd84",
+    "url": "/portfolio/static/js/runtime-main.860ade9b.js"
+  },
+  {
+    "revision": "cade15a6fc29b872e03b076c0be64766",
+    "url": "/portfolio/static/media/angular.cade15a6.png"
+  },
+  {
+    "revision": "52496625af2d0fd5f7e1f89635cb557b",
+    "url": "/portfolio/static/media/drive.52496625.png"
+  },
+  {
+    "revision": "957b72311aaac090d9d36f131526754b",
+    "url": "/portfolio/static/media/email.957b7231.png"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/portfolio/static/media/github.76d344f0.svg"
+  },
+  {
+    "revision": "5a83c1c25c1721c5142db6571f3f5035",
+    "url": "/portfolio/static/media/grad.5a83c1c2.jpeg"
+  },
+  {
+    "revision": "172f970466079737bed412743ec4a30b",
+    "url": "/portfolio/static/media/job-1.172f9704.jpeg"
+  },
+  {
+    "revision": "34946a3b7c993ee6596ab258551fd723",
+    "url": "/portfolio/static/media/job-2.34946a3b.jpg"
+  },
+  {
+    "revision": "3d40302b0864db8e2210e295152d8543",
+    "url": "/portfolio/static/media/mongo.3d40302b.png"
+  },
+  {
+    "revision": "f80a91ba03fbf0e99ea6949aa3a6399e",
+    "url": "/portfolio/static/media/mysql.f80a91ba.svg"
+  },
+  {
+    "revision": "9100f2eb47cae949fb1f01853263103c",
+    "url": "/portfolio/static/media/node.9100f2eb.png"
+  },
+  {
+    "revision": "2e11c90b9ca072329b987746adfb0da2",
+    "url": "/portfolio/static/media/phone.2e11c90b.png"
+  },
+  {
+    "revision": "960386ce5b8acb4794d14781b2389aca",
+    "url": "/portfolio/static/media/profile-0.960386ce.jpeg"
+  },
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "/portfolio/static/media/react.47ce6e77.png"
+  },
+  {
+    "revision": "2d37085f7c1256789738de35e6baab8f",
+    "url": "/portfolio/static/media/skype.2d37085f.png"
+  },
+  {
+    "revision": "a7d128d44079dd886358f74e8a428ffe",
+    "url": "/portfolio/static/media/whatsapp.a7d128d4.png"
+  }
+]);
