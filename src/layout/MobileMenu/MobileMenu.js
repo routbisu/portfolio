bc45ccb36e323747e9a63fdd95f7b0b7de211051
Biswaranjan Rout <biswaranjan.rout1989@gmail.com>
2020-03-07 18:09:51 +0800

    Fixed issues with Mobile menu

--- src/layout/MobileMenu/MobileMenu.js
+++ src/layout/MobileMenu/MobileMenu.js
@@ -4,28 +4,69 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './MobileMenu.scss';
 import { menuItems } from '../../config/appConfig';
 import { Link } from 'gatsby';
-import Logo from '../../images/logo.png';
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+
+let menuRef;
+let ccRef;
+let msRef;
 
 const MobileMenu = () => {
   const [open, setOpen] = useState(false);
 
+  const closeMenu = evt => {
+    if (
+      !(
+        menuRef &&
+        menuRef.contains(evt.target) &&
+        !msRef.contains(evt.target) &&
+        !ccRef.contains(evt.target)
+      )
+    ) {
+      setOpen(false);
+      document.removeEventListener('click', closeMenu);
+    }
+  };
+
+  const openMenu = () => {
+    document.addEventListener('click', closeMenu);
+    setOpen(true);
+  };
+
   return (
     <div className="mobile-menu-container mobile-only-strict">
-      <div className="hamburger-container" onClick={() => setOpen(true)}>
+      <div className="hamburger-container" onClick={openMenu}>
         <FontAwesomeIcon icon={faStream} className="hamburger" />
       </div>
 
-      <div className={`mobile-menu ${!open && 'menu-closed'}`}>
+      <div
+        className={`mobile-menu ${!open && 'menu-closed'}`}
+        ref={elem => {
+          menuRef = elem;
+        }}
+      >
         <div className="cross-container">
-          <div className="svg-container" onClick={() => setOpen(false)}>
+          <div
+            className="svg-container"
+            onClick={closeMenu}
+            ref={elem => {
+              ccRef = elem;
+            }}
+          >
             <FontAwesomeIcon icon={faTimes} />
           </div>
         </div>
 
-        <div className="menu-section">
+        <div
+          className="menu-section"
+          ref={elem => {
+            msRef = elem;
+          }}
+        >
+          <Link to="/" className="menu-item">
+            Home
+          </Link>
           {menuItems.map(item => (
-            <Link to={item.link} className="menu-item">
+            <Link to={item.link} className="menu-item" key={item.link}>
               {item.label}
             </Link>
           ))}
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,36 @@ import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 import './index.scss';
 import { Link } from 'gatsby';
 import { getLinkUrl } from '../services/commonService';
+import Slider from 'react-slick';
+
+const sliderConfig = {
+  arrows: false,
+  dots: true,
+  autoplay: true
+};
+
+const sliderContent = [
+  <p className="animated fadeIn">
+    I am a Web Developer.
+    <br />I am a self-motivated, and self-taught programmer, driven to build
+    amazing software that I am proud of.
+  </p>,
+  <p className="animated fadeIn">
+    I am a Frontend Developer.
+    <br />I have expertise on libraries like React, Vue &amp; Angular and a good
+    understanding of user experience (UX) principles.
+  </p>,
+  <p className="animated fadeIn">
+    I am a Polyglot Programmer.
+    <br />I have exposure to microservices, cloud services, monolithic
+    applications, relational &amp; document databases.
+  </p>,
+  <p className="animated fadeIn">
+    I am a Technical Lead.
+    <br />I have exposure to project management, estimation &amp; mentoring. I
+    work with tools like Jira, Confluence &amp; Bitbucket.
+  </p>
+];
 
 const IndexPage = () => {
   return (
@@ -22,11 +52,11 @@ const IndexPage = () => {
             <h1 className="animated fadeInDown">
               Hi, I am <span>Biswa.</span>
             </h1>
-            <p className="animated fadeIn">
-              I am a Web Developer.
-              <br />I am a self-motivated, and self-taught programmer, driven to
-              build amazing software that I am proud of.
-            </p>
+            <Slider {...sliderConfig}>
+              {sliderContent.map((content, i) => (
+                <div key={i}>{content}</div>
+              ))}
+            </Slider>
             <Link to={getLinkUrl('PROJECTS')}>
               <button className="see-more animated flipInY">
                 <span>See my work</span>
