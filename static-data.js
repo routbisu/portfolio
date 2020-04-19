26fad315d8234bdf056c16bfc70f0b69eab67f01
Biswaranjan Rout <biswaranjan.rout1989@gmail.com>
2020-04-19 15:32:08 +0800

    Quick fixes

--- static-data.js
+++ static-data.js
@@ -10,19 +10,16 @@ const skills = {
   js: { name: 'JavaScript', logo: 'js.svg', padding: 4 },
   mongodb: { name: 'Mongo DB', logo: 'mongodb.svg', padding: 4 },
   nodejs: { name: 'Node', logo: 'nodejs.svg', padding: 4 },
-  react: { name: 'React', logo: 'react.svg', showName: true, padding: 3 },
-  gatsby: { name: 'Gatsby', logo: 'gatsby.svg', showName: true, padding: 0 },
-  vuejs: { name: 'Vue.js', logo: 'vue.svg', showName: true, padding: 3 },
-  redux: { name: 'Redux', logo: 'redux.svg', showName: true, padding: 9 },
+  react: { name: 'React', logo: 'react.svg', padding: 3 },
+  gatsby: { name: 'Gatsby', logo: 'gatsby.svg', padding: 0 },
+  vuejs: { name: 'Vue.js', logo: 'vue.svg', padding: 3 },
+  redux: { name: 'Redux', logo: 'redux.svg', padding: 9 },
   sass: { name: 'SASS', logo: 'sass.svg', padding: 5 },
   typescript: {
     name: 'Typescript',
     logo: 'typescript.svg',
-    padding: 6,
-    showName: true
-  },
-  jest: { name: 'Jest' },
-  enzyme: { name: 'Enzyme' }
+    padding: 6
+  }
 };
 
 const linkTypes = {
@@ -51,18 +48,17 @@ const projects = {
         skills.typescript,
         skills.html5,
         skills.css3,
-        skills.nodejs,
-        skills.jest,
-        skills.enzyme
+        skills.nodejs
       ],
       description: [
-        `Wings suite is an internal-use only tool for automation of processes related to firmware management and testing on storage devices. It is a one-stop solution to setup and recreate validation environment, execute device management operations, and to perform automated failure analysis including features like perform firmware management operations, executes tests, diagnostic logs (collect, decode, visualize), create and execute test cases. Its target users include firmware developers, firmware validation engineers and failure analysis engineers.`
+        `Wings Suite is an internal product for automation of processes related to firmware management & testing for storage devices. It enables firmware engineers to test storage devices from a web based interface in contrast to the existing system where they use command line tools to run these tests. Wings also allows them to save test suites, decode test results, manage properties of storage devices and perform failure analysis all from a user-friendly web portal.`
       ],
       responsibilities: [
-        `Lead the UI design team to create mockups from scratch followed by end to end development of features.`,
-        `Architect a scalable UI development model for React.js with central state management using Redux.`,
-        `Consuming REST APIs from the backend flask server which interacts directly with storage devices.`,
-        `Writing unit tests for react components using Jest and Enzyme.`
+        `Hired and managed a small team of frontend engineers & QA engineers.`,
+        `Architected a scalable fronted development platform using React and Redux.`,
+        `Developed mockups and performed user-experience studies with stakeholders.`,
+        `Mentored two interns & trained them on Frontend development principles.`,
+        `Developed guidlines for CI/CD & automated unit testing for the project.`
       ]
     },
     { title: 'XInsure', slug: 'xinsure' },
